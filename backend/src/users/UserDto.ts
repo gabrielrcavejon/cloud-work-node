b7b7e021f264ee52b162922eeda49c3302439329
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2024-04-07 22:30:35 -0300

    prisma and tabzise

--- backend/src/users/UserDto.ts
+++ backend/src/users/UserDto.ts
@@ -1,7 +1,7 @@
 import { IsString, IsNotEmpty } from 'class-validator'
 
 export class UserDto {
-   @IsString()
-   @IsNotEmpty()
-   name: string;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -4,70 +4,70 @@ import { UserDto } from './UserDto';
 
 @Injectable()
 export class UsersService {
-   private users: User[] = [
-      {
-         id: 1,
-         name: "gabriel"
-      },
-      {
-         id: 2,
-         name: "Pedro"
-      }
-   ]
+  private users: User[] = [
+    {
+      id: 1,
+      name: "gabriel"
+    },
+    {
+      id: 2,
+      name: "Pedro"
+    }
+  ]
 
-   findAll() {
-      return this.users.filter(Boolean);
-   }
+  findAll() {
+    return this.users.filter(Boolean);
+  }
 
-   async findByid(id: number) {
-      const user = this.users.find(users => users?.id === id);
+  async findByid(id: number) {
+    const user = this.users.find(users => users?.id === id);
 
-      if (!user) {
-         throw Error(`Mensagem com o ID ${id} não encontrado.`);
-      }
+    if (!user) {
+      throw Error(`Mensagem com o ID ${id} não encontrado.`);
+    }
 
-      return user;
-   }
+    return user;
+  }
 
-   create(userDto: UserDto) {
-      const id = this.users.length + 1;
+  create(userDto: UserDto) {
+    const id = this.users.length + 1;
 
-      const user: User = {
-         id,
-         ...userDto
-      }
+    const user: User = {
+      id,
+      ...userDto
+    }
 
-      this.users.push(user);
+    this.users.push(user);
 
-      return user;
-   }
+    return user;
+  }
 
-   async update(id: number, userDto: UserDto) {
-      const index = this.users.findIndex((user) => user?.id === id);
+  async update(id: number, userDto: UserDto) {
+    const index = this.users.findIndex((user) => user?.id === id);
 
-      if (index < 0) {
-         throw Error(`Usuario com o id '${id}' não encontrada.`);
-      }
+    if (index < 0) {
+      throw Error(`Usuario com o id '${id}' não encontrada.`);
+    }
 
-      const user: User = {
-         id,
-         ...userDto
-      }
+    const user: User = {
+      id,
+      ...userDto
+    }
 
-      this.users[index] = user;
+    this.users[index] = user;
 
-      return user;
-   }
+    return user;
+  }
 
-   async delete(id: number) {
-      const index = this.users.findIndex((user) => user?.id === id);
+  async delete(id: number) {
+    const index = this.users.findIndex((user) => user?.id === id);
 
-      if (index < 0) {
-         throw Error(`Usuario com o id '${id}' não encontrada.`);
-      }
+    if (index < 0) {
+      throw Error(`Usuario com o id '${id}' não encontrada.`);
+    }
 
-      delete this.users[index];
+    delete this.users[index];
 
-      return true;
-   }
+    return true;
+  }
 }
